3195a7edb5864a55e73a2a11f3e113a8e07826ab
sergethompson <serge.thompson@gmail.com>
2014-01-09 10:19:36 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TunrApp::Application.config.secret_token = 'e8baa9e72b3eac0dcbff7351fa31e60c15e8bc9b3f57f03f4af4ecb293511ccafc4b6b38025b40d42b1d599e3e88f3a38c62fb6a617f15d2156d637309292c91'
